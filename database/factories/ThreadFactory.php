c0756b33b4f92334287521c67ee92602b2267125
Midstackdev <midstackdev@gmail.com>
2020-03-05 23:11:26 +0000

    Initial Seeding

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Models\Thread;
+use App\Models\User;
+use Faker\Generator as Faker;
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+        	return factory(User::class)->create()->id;
+        }, 
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
